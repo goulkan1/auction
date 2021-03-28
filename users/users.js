5d64544f1c97861d8198d89b432d3a7f75fc124a
goulkan <landongkista@email.unikom.ac.id>
2021-03-29 00:48:55 +0700

    'cors'

--- users/users.js
+++ users/users.js
@@ -10,7 +10,6 @@ require("./src/models/User");
 const auth = require("./src/middleware/auth");
 const multer = require("multer");
 const { validationResult } = require("express-validator");
-var path = require("path");
 const User = mongoose.model("User");
 app.use("/img", express.static("img"));
 app.use("/logo", express.static("logo"));
@@ -26,13 +25,19 @@ mongoose.connect(
   console.log("konek database")
 );
 
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Credentials', true);
-//   res.header('Access-Control-Allow-Origin', req.headers.origin);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
